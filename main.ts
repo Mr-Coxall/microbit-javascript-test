63e532fe0d83a536e6e0ebe99532e21cb8a26389
Patrick Coxall <mr.coxall@mths.ca>
2023-06-25 09:30:54 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+// comment
+
+let counter = 0
+basic.showNumber(0)
+
+while (counter < 5) {
+  basic.showNumber(counter)
+  console.log(counter)
+  counter++
+}
+
+while (true) {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(0)
+    }
+}
